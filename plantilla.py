4c33d57fa259b90b2998492a5bbf32543eb03b1f
Miguel-Estradam <miguelestradam12@gmail.com>
2023-03-09 23:33:56 -0500

    first commit

---
+++ plantilla.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+from pygame.locals import *
+
+pygame.init()
+
+ventana = pygame.display.set_mode((400,300))
+pygame.display.set_caption("My game") #titulo
+colorFondo = ()
